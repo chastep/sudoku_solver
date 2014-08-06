6e5294fb7958de82768723983eb6250718f2d465
Torey Hickman <torey@toreyhickman.com>
2014-08-06 09:28:00 -0500

    Refactor the runner file
    The comments directing students were condensed
    into one comment block, appearing before the
    driver code.

--- source/runner.rb
+++ source/runner.rb
@@ -1,14 +1,20 @@
-#This file reads in a file with several sample boards.
-
 require_relative 'sudoku'
 
-# The file has newlines at the end of each line, so we call
-# String#chomp to remove them.
-board_string = File.readlines('sample.unsolved.txt').first.chomp
+# The sudoku puzzles that your program will solve can be found
+# in the sudoku_puzzles.txt file.
+#
+# Currently, Line 16 defines the variable board_string to equal
+# the first puzzle (i.e., the first line in the .txt file).
+# After your program can solve this first puzzle, edit
+# the code below, so that the program tries to solve
+# all of the puzzles.
+#
+# Remember, the file has newline characters at the end of each line,
+# so we call String#chomp to remove them.
+
+
+board_string = File.readlines('sudoku_puzzles.txt').first.chomp
 
 game = Sudoku.new(board_string)
-
-# Remember: this will just fill out what it can and not "guess"
 game.solve!
-
-puts game.to_s
+puts game
