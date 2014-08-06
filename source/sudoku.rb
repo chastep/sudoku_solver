95baef75e85c4712599fd33e7d95b45e3ea7cdde
Torey Hickman <torey@toreyhickman.com>
2014-08-06 09:14:47 -0500

    Simplify comment describing the Sudoku#to_s method
    I removed the direction to students not to spend
    too much time writing the #to_s method and
    to flag staff if they are having trouble.  Seems
    unnecessary.

--- source/sudoku.rb
+++ source/sudoku.rb
@@ -9,9 +9,6 @@ class Sudoku
   end
 
   # Returns a string representing the current state of the board
-  # Don't spend too much time on this method; flag someone from staff
-  # if you are.
   def to_s
-
   end
 end
